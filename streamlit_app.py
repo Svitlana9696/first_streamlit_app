a04a3b5bbd635d1aa961b8117decb12558226389
Svitlana9696 <58072877+Svitlana9696@users.noreply.github.com>
2022-11-08 18:30:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchone()
 streamlit.header("The fruit load list contains: ")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 
